fd4ecbe2243f7b5557813c1a822ed9e5bfe679ff
vpanic <vince@pale.io>
2023-03-03 15:12:53 -0500

    abstracts db anonlogin to firebase.js; works

--- src/components/user-status.vue
+++ src/components/user-status.vue
@@ -1,25 +1,20 @@
 <template>
     <div class="user-status">
-        <span v-if="user">
-            Welcome back, {{ user || name || "-" }}
-        </span>
-        <span v-if="!user">
             <ui-button text="Sign In" @click="logUserIn()"></ui-button>
-        </span>
     </div>
 </template>
 
 <script setup>
-
 import { useCurrentUser } from 'vuefire'
 import UiButton from '@/components/ui-button.vue';
-
+import { dbTools } from "@/firebase";
 const user = useCurrentUser();
-// console.log("db",db);
-// console.log("auth",auth);
 
 function logUserIn(){
-    console.log("what..?");
+    console.log("Loggin user in?");
+    console.table(user);
+    dbTools.log_in_anon();
+    console.table(user);
 }
 
 </script>
--- src/firebase.js
+++ src/firebase.js
@@ -1,23 +1,26 @@
-import { initializeApp } from 'firebase/app'
-// eslint-disable-next-line no-unused-vars
-import { getFirestore, collection } from 'firebase/firestore'
-import { useFirebaseAuth } from 'vuefire';
-// import { createApp } from 'vue'
-// import { useCollection } from 'vuefire'
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { useFirebaseAuth } from "vuefire";
+import { signInAnonymously, getAuth } from "firebase/auth";
 
- initializeApp({
+const firebaseApp = initializeApp({
   apiKey: "AIzaSyB5-u_pSw50BvlJPCKp27lw1wKj-_pJ7oM",
   authDomain: "vue-project-c3cb5.firebaseapp.com",
   projectId: "vue-project-c3cb5",
   storageBucket: "vue-project-c3cb5.appspot.com",
   messagingSenderId: "67183728791",
   appId: "1:67183728791:web:04f02452fbfd031c2d31d2",
-  measurementId: "G-QT3MY7806H"
-})
+  measurementId: "G-QT3MY7806H",
+});
 
 const db = getFirestore();
 const auth = useFirebaseAuth();
+const firebaseAuth = getAuth(firebaseApp);
 
-export { db, auth };
+const dbTools = {
+  log_in_anon: function () {
+    return signInAnonymously(firebaseAuth);
+  },
+};
 
-
+export { firebaseApp, db, auth, dbTools };
